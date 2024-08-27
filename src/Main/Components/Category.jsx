c8b7582bbec100b34294a911e8d29d4b747c9356
Alan <alanfoureyes@gmail.com>
2024-08-27 22:35:07 +0300

    automation of category selection

--- src/Main/Components/Category.jsx
+++ src/Main/Components/Category.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
 
 export const Category = () => {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+
+  const categories = [
+    'Все',
+    'Мясные',
+    'Вегетарианская',
+    'Гриль',
+    'Острые',
+    'Закрытые',
+  ];
+
+  const onClickCategory = index => {
+    setActiveIndex(index);
+  };
+
   return (
     <div className="categories">
       <ul>
-        <li class="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map((value, i) => (
+          <li
+            onClick={() => onClickCategory(i)}
+            className={activeIndex === i ? 'active' : ''}
+          >
+            {value}
+          </li>
+        ))}
       </ul>
     </div>
   );
