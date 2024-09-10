15a2e1d5eeb91b621b3bd0937a9eb72a1dbd75b0
Alan <alanfoureyes@gmail.com>
2024-09-10 21:29:44 +0300

    integration with fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Header } from './Components/Header';
 import { Main } from './Main/Main';
 import './scss/app.scss';
+
 export const App = () => {
   return (
     <div className="wrapper">
       <Header />
+
       <Main />
     </div>
   );
--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -7,6 +7,23 @@ import { PizzaBlock } from './Components/PizzaBlock';
 import { Sort } from './Components/Sort';
 
 export const Main = () => {
+  const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    setIsLoading(true);
+    fetch('https://66e05a9c2fb67ac16f2953b1.mockapi.io/items')
+      .then(res => res.json())
+      .then(arr => {
+        setItems(arr);
+        setIsLoading(false);
+      })
+      .catch(error => {
+        console.error('Ошибка при загрузке данных:', error);
+        setIsLoading(false);
+      });
+  }, []);
+
   console.log(pizzas);
   return (
     <div className="content">
@@ -17,9 +34,11 @@ export const Main = () => {
         </div>
         <h2 className="content__title">Все пиццы</h2>
         <div className="content__items">
-          {pizzas.map(obj => (
-            <PizzaBlock key={obj.id} {...obj} />
-          ))}
+          {isLoading ? (
+            <p>Загрузка пицц...</p>
+          ) : (
+            items.map(obj => <PizzaBlock key={obj.id} {...obj} />)
+          )}
         </div>
       </div>
     </div>
