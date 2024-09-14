d71fd62396a3e9fad188e4ab28c44a5a5c5c0f11
Alan <alanfoureyes@gmail.com>
2024-09-14 23:14:48 +0300

    react router@6

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
+import { Route, Routes } from 'react-router';
 import { Header } from './Components/Header';
 import { Main } from './Main/Main';
+import { CartEmpty } from './pages/CartEmpty';
+import { NotFound } from './pages/NotFound/NotFound';
 import './scss/app.scss';
 
 export const App = () => {
@@ -7,7 +10,12 @@ export const App = () => {
     <div className="wrapper">
       <Header />
 
-      <Main />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="cart" element={<CartEmpty />} />
+        {/* <Main /> */}
+      </Routes>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,29 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
-
-// const newPost = {
-//   userId: 1,
-//   id: 11111,
-//   title: 'hello',
-//   body: 'vsbfdbf',
-// };
-
-// fetch('https://jsonplaceholder.typicode.com/posts', {
-//   method: 'POST',
-//   body: JSON.stringify(newPost),
-// })
-//   .then(data => {
-//     return data.json();
-//   })
-//   .then(info => {
-//     console.log(info);
-//   });
